454f6b854d7c146bed540c82bd21675e69fd31e5
Sobinjm <jmsobin@gmail.com>
2023-12-01 14:25:17 +0530

    Configured Flowbite

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,12 @@
+import { Button } from "flowbite-react";
 
 
 export  default function Home(){
 
     return (
+        <>
         <p>welcome all</p>
+        <Button>Demo </Button>
+        </>
     )
 }
