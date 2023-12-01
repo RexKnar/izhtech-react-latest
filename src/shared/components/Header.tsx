5027693b0f84d7bb788246a8c35201c6e39d4c7d
Heena Shahnaz <heenashahnaz20@gmail.com>
2023-12-01 15:22:46 +0530

    first commit

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -4,6 +4,7 @@ import { MenuItem } from "./types/MenuItems";
 export default function Header(){
     return (
         <header> <nav>
+            
             <ul>
                 {menuItems.map((menu:MenuItem)=>{
                     return (
