08151aabd80d06ab5e03217c162923ee930ba6cc
Heena Shahnaz <heenashahnaz20@gmail.com>
2023-12-04 15:45:30 +0530

    Feature Section

--- src/shared/components/CustomFooter.tsx
+++ src/shared/components/CustomFooter.tsx
@@ -1,18 +1,18 @@
 import { Footer } from "flowbite-react";
 import { Button, Checkbox, TextInput } from "flowbite-react";
-import image from "/Manoj/Izh-Tech New/izhtech-react-latest/src/assets/Logo/Logo-white.png";
+import logo from "../../assets/Logo/Logo-white.png";
 
 export default function CustomFooter() {
   return (
     <footer className="bg-neutral-900 ">
       <div className="container ">
-        <div className="grid w-full lg:grid-cols-4 gap-8 p-8 lg:px-28  md:px-6 py-8 md:grid-cols-4">
+        <div className="grid w-full gap-8 p-8 py-8 lg:grid-cols-4 lg:px-28 md:px-6 md:grid-cols-4">
           <div>
-            <img src={image} className="h-16" alt="" />
+            <img src={logo} className="h-16" alt="" />
           </div>
           <div className="text-stone-50 ">
             <h2 className="font-semibold">Singapore</h2>
-            <div className="space-y-1 pt-3 text-gray-300">
+            <div className="pt-3 space-y-1 text-gray-300">
               <p>Head Office</p>
               <p>Flora Drive, Singapore</p>
               <p>506889.</p>
@@ -20,18 +20,18 @@ export default function CustomFooter() {
           </div>
           <div className="text-stone-50 ">
             <h2 className="font-semibold">Work Inquiries</h2>
-            <div className="space-y-1 pt-3 text-gray-300">
+            <div className="pt-3 space-y-1 text-gray-300">
               <p>Interested in working with us?</p>
               <p>hello@izhtech.com</p>
             </div>
           </div>
           <div>
-            <h2 className="text-stone-50 font-semibold">
+            <h2 className="font-semibold text-stone-50">
               Sign up for the newsletter
             </h2>
             <Footer.LinkGroup col>
               <div className="max-w-md">
-                <div className=" flex fle-wrap pt-3">
+                <div className="flex pt-3 fle-wrap">
                   <TextInput
                     className="text-slate-800"
                     id="email"
@@ -45,7 +45,7 @@ export default function CustomFooter() {
                   </Button>
                 </div>
 
-                <div className="mt-4 gap-2 flex flex-row">
+                <div className="flex flex-row gap-2 mt-4">
                   <Checkbox
                     id="accept"
                     style={{ backgroundColor: "#201f23", borderColor: "grey" }}
@@ -60,14 +60,14 @@ export default function CustomFooter() {
             </Footer.LinkGroup>
           </div>
         </div>
-        <div className="w-full bg-neutral-900 p-8 lg:px-28 sm:px-4 py-6 sm:flex sm:items-center sm:justify-between">
+        <div className="w-full p-8 py-6 bg-neutral-900 lg:px-28 sm:px-4 sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
             by="izh tech 2023. All rights reserved"
             className="text-stone-50"
           />
-          <div className="mt-4 flex space-x-6 sm:mt-0 text-stone-50 sm:justify-center list-none">
-            <Footer.Link className="text-stone-50 text-xs text-gray-300" href="#">
+          <div className="flex mt-4 space-x-6 list-none sm:mt-0 text-stone-50 sm:justify-center">
+            <Footer.Link className="text-xs text-gray-300 text-stone-50" href="#">
               Sitemap
             </Footer.Link>
             <Footer.Link href="#" className="text-xs text-gray-300">
