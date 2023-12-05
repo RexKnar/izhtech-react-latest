86903015ccca23ec58c4b4de8752ac67c5688895
Heena Shahnaz <heenashahnaz20@gmail.com>
2023-12-05 16:04:29 +0530

    partner company section completed

--- src/pages/home/components/partnercompany.tsx
+++ src/pages/home/components/partnercompany.tsx
@@ -1,24 +1,24 @@
-export default function Slider() {
+export default function partnercompany () {
     return (
       <section className="grid w-full bg-white">
         <div className="flex flex-col w-full py-8 md:flex-row md:py-24 lg:flex-row lg:flex-wrap lg:justify-center">
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/spotify.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/spotify.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/walmart.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/walmart.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/amazon.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/amazon.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/intel.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/intel.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/airbnb.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/airbnb.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
           <div className="flex flex-col items-center py-2 md:flex-1 lg:w-1/4">
-            <img src="/images/slider/linkedin.png" alt="Image" className="w-auto h-8 mb-2" />
+            <img src="/images/partnercompany/linkedin.png" alt="Image" className="w-auto h-8 mb-2" />
           </div>
         </div>
       </section>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import Feature from "./components/Feature";
-import Slider from "./components/Slider";
+import Slider from "./components/partnercompany";
 export  default function Home(){
 
     return (
